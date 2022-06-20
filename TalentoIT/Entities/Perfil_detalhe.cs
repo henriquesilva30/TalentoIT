6c48aaab6976d047e6c7740aa5b2c46921baaa26
Joel Silva <henriquesa@ipvc.pt>
2022-06-20 01:28:58 +0100

    update

---
+++ TalentoIT/Entities/Perfil_detalhe.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TalentoIT.Entities
+{
+    [Table("perfil_detalhe")]
+    public partial class Perfil_detalhe
+    {
+        public Perfil_detalhe()
+        {
+            Detalhes = new HashSet<Talento_skill>();
+            
+        }
+
+        [Key] 
+        [Column("id_perfil_detalhe")] 
+        public int Perfil_detalheId { get; set; }
+
+        [Required]
+        [Column("titulo_exp")]
+        [StringLength(100)]
+        public string Expr { get; set; }
+
+        [Required]
+        [Column("nome_empresa")]
+        [StringLength(100)]
+        public string Empresa { get; set; }
+        
+        [Required]
+        [Column("ano_inicio")]
+        [StringLength(100)] 
+        public string Ano_Inicio { get; set; }
+        
+        [Required]
+        [Column("ano_fim")]
+        [StringLength(100)]
+        public string Ano_Fim { get; set; }
+
+        public int? Perfil_talentoId {  get; set; }
+
+        [ForeignKey(nameof(Perfil_talentoId))]
+        [InverseProperty("Perfil_talento")] 
+        public virtual Perfil_talento Perfil_talento { get; set; }
+
+        [InverseProperty(nameof(Talento_skill.Perfil_detalhe))]
+        public virtual ICollection<Talento_skill> Detalhes { get; set; }
+
+
+        //  [InverseProperty(nameof(Meal.User))] public virtual ICollection<Meal> Meals { get; set; }
+    }
+}
---
+++ TalentoIT/Entities/Proposta_user.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace TalentoIT.Entities
+{
+         [Table("proposta_user")]
+         public partial class Proposta_user
+         {
+             public Proposta_user()
+             {
+                 //Meals = new HashSet<Meal>();
+                 
+             }
+     
+             [Key] 
+             [Column("id_proposta_user")] 
+             public int Proposta_userId { get; set; }
+ 
+             public int? UserId { get; set; }
+             [ForeignKey(nameof(UserId))]
+             [InverseProperty("User")] 
+             public virtual User User { get; set; }
+            
+            public int? PropostaId { get; set; }
+            [ForeignKey(nameof(PropostaId))]
+            [InverseProperty("Proposta")] 
+            public virtual Proposta Proposta { get; set; }
+     
+     
+     
+             //  [InverseProperty(nameof(Meal.User))] public virtual ICollection<Meal> Meals { get; set; }
+         }
+}
